02a84fa43a72e4f80d0b52d2acff06dc7da0dfed
John Steele <camusicjunkie@yahoo.com>
2018-05-28 18:43:55 -0500

    Added support for user.getWeeklyAlbumChart method

---
+++ PowerLFM/Public/Get-LFMUserWeeklyAlbumChart.ps1
@@ -0,0 +1,60 @@
+function Get-LFMUserWeeklyAlbumChart {
+    [CmdletBinding()]
+    [OutputType('PowerLFM.User.WeeklyAlbumChart')]
+    param (
+        [Parameter(Mandatory,
+                   ValueFromPipelineByPropertyName)]
+        [string] $UserName,
+
+        [Parameter(ValueFromPipelineByPropertyName)]
+        [string] $From,
+
+        [Parameter(ValueFromPipelineByPropertyName)]
+        [string] $To
+    )
+
+    begin {
+        #Default hashtable
+        $apiParams = [ordered] @{
+            'method' = 'user.getWeeklyAlbumChart'
+            'api_key' = $LFMConfig.APIKey
+            'format' = 'json'
+        }
+    }
+    process {
+        switch ($PSBoundParameters.Keys) {
+            'UserName' {$apiParams.add('user', $UserName)}
+            'From' {$apiParams.add('from', $From)}
+            'To' {$apiParams.add('to', $To)}
+        }
+
+        #Building string to append to base url
+        $keyValues = $apiParams.GetEnumerator() | ForEach-Object {
+            "$($_.Name)=$($_.Value)"
+        }
+        $string = $keyValues -join '&'
+
+        $apiUrl = "$baseUrl/?$string"
+    }
+    end {
+        $iwr = Invoke-WebRequest -Uri $apiUrl
+        $jsonString = $iwr.AllElements[3].innerHTML
+        $hash = $jsonString | ConvertFrom-Json | ConvertTo-HashTable
+         
+        foreach ($album in $hash.WeeklyAlbumChart.Album) {
+            $albumInfo = [pscustomobject] @{
+                'Album' = $album.Name
+                'Url' = $album.Url
+                'Id' = $album.Mbid
+                'Artist' = $album.Artist.'#text'
+                'ArtistId' = $album.Artist.Mbid
+                'PlayCount' = $album.PlayCount
+                'From' = ConvertFrom-UnixTime -UnixTime $hash.WeeklyAlbumChart.'@attr'.From -Local
+                'To' = ConvertFrom-UnixTime -UnixTime $hash.WeeklyAlbumChart.'@attr'.To -Local
+            }
+
+            $albumInfo.PSObject.TypeNames.Insert(0, 'PowerLFM.User.WeeklyChartList')
+            Write-Output $albumInfo
+        }
+    }
+}
