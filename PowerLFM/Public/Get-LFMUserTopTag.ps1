60bd33d3210a20fbb180d42e19fb6e2e7b2bbc46
John Steele <camusicjunkie@yahoo.com>
2018-05-28 18:42:34 -0500

    Added support for user.getTopTags method

---
+++ PowerLFM/Public/Get-LFMUserTopTag.ps1
@@ -0,0 +1,54 @@
+function Get-LFMUserTopTag {
+    [CmdletBinding()]
+    [OutputType('PowerLFM.User.TopTag')]
+    param (
+        [Parameter(Mandatory,
+                   ValueFromPipelineByPropertyName)]
+        [string] $UserName,
+
+        [string] $Limit
+    )
+
+    begin {
+        #Default hashtable
+        $apiParams = [ordered] @{
+            'method' = 'user.getTopTags'
+            'api_key' = $LFMConfig.APIKey
+            'format' = 'json'
+        }
+
+        #Adding key/value to hashtable based off optional parameters
+        switch ($PSBoundParameters.Keys) {
+            'Limit' {$apiParams.add('limit', $Limit)}
+        }
+    }
+    process {
+        switch ($PSBoundParameters.Keys) {
+            'UserName' {$apiParams.add('user', $UserName)}
+        }
+
+        #Building string to append to base url
+        $keyValues = $apiParams.GetEnumerator() | ForEach-Object {
+            "$($_.Name)=$($_.Value)"
+        }
+        $string = $keyValues -join '&'
+
+        $apiUrl = "$baseUrl/?$string"
+    }
+    end {
+        $iwr = Invoke-WebRequest -Uri $apiUrl
+        $jsonString = $iwr.AllElements[3].innerHTML
+        $hash = $jsonString | ConvertFrom-Json | ConvertTo-HashTable
+         
+        foreach ($tag in $hash.TopTags.Tag) {
+            $tagInfo = [pscustomobject] @{
+                'Tag' = $tag.Name
+                'TagUrl' = $tag.url
+                'Count' = $tag.Count
+            }
+
+            $tagInfo.PSObject.TypeNames.Insert(0, 'PowerLFM.User.TopTag')
+            Write-Output $tagInfo
+        }
+    }
+}
