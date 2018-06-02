96f88c959f6ea012b23c4207563f09a2ab87a88a
John Steele <camusicjunkie@yahoo.com>
2018-06-02 01:09:01 -0600

    Added support for album.addTags method

---
+++ PowerLFM/Public/Get-LFMAlbumTag.ps1
@@ -0,0 +1,85 @@
+function Get-LFMAlbumTag {
+    [CmdletBinding(DefaultParameterSetName = 'album')]
+    [OutputType('PowerLFM.Album.UserTag')]
+    param (
+        [Parameter(Mandatory,
+                   ValueFromPipelineByPropertyName,
+                   ParameterSetName = 'album')]
+        [string] $Album,
+
+        [Parameter(Mandatory,
+                   ValueFromPipelineByPropertyName,
+                   ParameterSetName = 'album')]
+        [string] $Artist,
+
+        [Parameter(ValueFromPipelineByPropertyName,
+                   ParameterSetName = 'id')]
+        [string] $Id,
+        [string] $UserName,
+        [switch] $AutoCorrect
+    )
+
+    begin {
+        #Default hashtable
+        $apiParams = [ordered] @{
+            'method' = 'album.getTags'
+            'format' = 'json'
+        }
+
+        switch ($PSBoundParameters.Keys) {
+            'AutoCorrect' {$apiParams.add('autocorrect', 1)}
+        }
+    }
+    process {
+        #Adding key/value to hashtable based off ParameterSetName
+        switch ($PSCmdlet.ParameterSetName) {
+            'album' {$apiParams.add('album', $Album);
+                     $apiParams.add('artist', $Artist)}
+            'id'    {$apiParams.add('mbid', $Id)}
+        }
+
+        #Adding key/value to hashtable based off optional parameters
+        if ($PSBoundParameters.ContainsKey('UserName')) {
+            $apiParams.add('user', $UserName)
+            $apiParams.add('api_key', $LFMConfig.APIKey)
+        }
+        else {
+            $apiParams.add('api_key', $LFMConfig.APIKey)
+            $apiParams.add('sk', $LFMConfig.SessionKey)
+        }
+        
+        #Building string to append to base url
+        $keyValues = $apiParams.GetEnumerator() | ForEach-Object {
+            "$($_.Name)=$($_.Value)"
+        }
+        $string = $keyValues -join '&'
+
+        $apiUrl = "$baseUrl/?$string"
+    }
+    end {
+        $iwr = Invoke-WebRequest -Uri $apiUrl
+        $jsonString = $iwr.AllElements[3].innerHTML
+        $hash = $jsonString | ConvertFrom-Json | ConvertTo-HashTable
+
+        $tags = foreach ($tag in $hash.Tags.Tag) {
+            $tagInfo = [pscustomobject] @{
+                'Tag' = $tag.Name
+                'Url' = $tag.Url
+            }
+            $tagInfo.PSObject.TypeNames.Insert(0, 'PowerLFM.Album.Tag')
+            Write-Output $tagInfo
+        }
+
+        $artistTagInfo = [pscustomobject] @{
+            'Artist' = $hash.Tags.'@attr'.Artist
+            'Tags' = $tags
+        }
+
+        if ($PSBoundParameters.ContainsKey('UserName')) {
+            $artistTagInfo | Add-Member -MemberType NoteProperty -Name 'UserName' -Value $UserName
+        }
+
+        $artistTagInfo.PSObject.TypeNames.Insert(0, 'PowerLFM.Album.UserTag')
+        Write-Output $artistTagInfo
+    }
+}
