fb1ea4da2b03a90e89b98f79831d223a2bc6b7c4
John Steele <camusicjunkie@yahoo.com>
2018-06-02 00:45:51 -0600

    Added album signature private function

---
+++ PowerLFM/Private/New-LFMAlbumSignature.ps1
@@ -0,0 +1,45 @@
+function New-LFMAlbumSignature {
+    [CmdletBinding()]
+    [OutputType('System.String')]
+    param (
+        [Parameter(Mandatory)]
+        [ValidateNotNullOrEmpty()]
+        [ValidateSet('album.addTags','album.removeTag')]
+        [string] $Method,
+
+        [Parameter(Mandatory)]
+        [ValidateNotNullOrEmpty()]
+        [string] $Album,
+
+        [Parameter(Mandatory)]
+        [ValidateNotNullOrEmpty()]
+        [string] $Artist,
+
+        [Parameter(Mandatory)]
+        [ValidateNotNullOrEmpty()]
+        [ValidateCount(1,10)]
+        [string[]] $Tag
+    )
+    try {
+        $sigParams = @{
+            'method' = $Method
+            'api_key' = $LFMConfig.ApiKey
+            'sk' = $LFMConfig.SessionKey
+            'album' = $Album
+            'artist' = $Artist
+            'tags' = $Tag
+        }
+    
+        $keyValues = $sigParams.GetEnumerator() | Sort-Object Name | ForEach-Object {
+            "$($_.Key)$($_.Value)"
+        }
+    
+        $string = $keyValues -join ''
+    
+        Get-Md5Hash -String "$string$($LFMConfig.SharedSecret)"
+        Write-Verbose "$string$($LFMConfig.SharedSecret)"
+    }
+    catch {
+        Write-Error $_.Exception.Message
+    }
+}
