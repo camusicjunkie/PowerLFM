e3fb202ab049b43776bbf684078aeefc2666c4fe
John Steele <camusicjunkie@yahoo.com>
2018-06-13 22:08:28 -0600

    Added build.ps1 script

---
+++ build.ps1
@@ -0,0 +1,10 @@
+[cmdletbinding()]
+param(
+    [string[]]$Task = 'default'
+)
+
+if (-not (Get-Module -Name Pester -ListAvailable)) { Install-Module -Name Pester -Scope CurrentUser }
+if (-not (Get-Module -Name psake -ListAvailable)) { Install-Module -Name psake -Scope CurrentUser }
+if (-not (Get-Module -Name PSDeploy -ListAvailable)) { Install-Module -Name PSDeploy -Scope CurrentUser }
+
+Invoke-Psake -buildFile "$PSScriptRoot\psake.ps1" -taskList $Task -Verbose:$VerbosePreference
