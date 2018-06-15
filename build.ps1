99c8ff9beeb23734bf7779bb7c71cdb7774ce22d
John Steele <camusicjunkie@yahoo.com>
2018-06-15 00:14:39 -0600

    small tweaks

--- build.ps1
+++ build.ps1
@@ -8,6 +8,6 @@ Get-PackageProvider -Name NuGet -ForceBootstrap | Out-Null
 if (-not (Get-Module -Name Pester -ListAvailable)) { Install-Module -Name Pester -Scope CurrentUser -Force }
 if (-not (Get-Module -Name psake -ListAvailable)) { Install-Module -Name psake -Scope CurrentUser -Force}
 if (-not (Get-Module -Name PSDeploy -ListAvailable)) { Install-Module -Name PSDeploy -Scope CurrentUser -Force}
-if (-not (Get-Module -Name PSScriptAnalyzer -ListAvailable)) { Install-Module -Name PSDeploy -Scope CurrentUser -Force}
+if (-not (Get-Module -Name PSScriptAnalyzer -ListAvailable)) { Install-Module -Name PSScriptAnalyzer -Scope CurrentUser -Force}
 
 Invoke-Psake -buildFile "$PSScriptRoot\psake.ps1" -taskList $Task -Verbose:$VerbosePreference
