1dddaf678f3642def0fb67d4006c48dd139465a6
John Steele <camusicjunkie@yahoo.com>
2018-06-14 20:38:18 -0600

    Added force switch

--- build.ps1
+++ build.ps1
@@ -3,8 +3,8 @@ param(
     [string[]]$Task = 'default'
 )
 
-if (-not (Get-Module -Name Pester -ListAvailable)) { Install-Module -Name Pester -Scope CurrentUser }
-if (-not (Get-Module -Name psake -ListAvailable)) { Install-Module -Name psake -Scope CurrentUser }
-if (-not (Get-Module -Name PSDeploy -ListAvailable)) { Install-Module -Name PSDeploy -Scope CurrentUser }
+if (-not (Get-Module -Name Pester -ListAvailable)) { Install-Module -Name Pester -Scope CurrentUser -Force }
+if (-not (Get-Module -Name psake -ListAvailable)) { Install-Module -Name psake -Scope CurrentUser -Force}
+if (-not (Get-Module -Name PSDeploy -ListAvailable)) { Install-Module -Name PSDeploy -Scope CurrentUser -Force}
 
 Invoke-Psake -buildFile "$PSScriptRoot\psake.ps1" -taskList $Task -Verbose:$VerbosePreference
