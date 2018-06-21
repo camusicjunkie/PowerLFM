3c6b9d791c2b4b3fa29b97cd98dd7e3d15a318fc
John Steele <camusicjunkie@yahoo.com>
2018-06-21 00:26:55 -0600

    Small tweaks

--- PowerLFM/Public/Get-LFMGeoTopArtist.ps1
+++ PowerLFM/Public/Get-LFMGeoTopArtist.ps1
@@ -2,6 +2,11 @@ function Get-LFMGeoTopArtist {
     [CmdletBinding()]
     [OutputType('PowerLFM.Geo.TopArtists')]
     param (
+        [Parameter(Mandatory,
+                   ValueFromPipeline,
+                   ValueFromPipelineByPropertyName)]
+        [string] $Country,
+
         [Parameter()]
         [ValidateRange(1,119)]
         [string] $Limit,
@@ -9,40 +14,47 @@ function Get-LFMGeoTopArtist {
         [string] $Page
     )
 
-    #Default hashtable
-    $apiParams = [ordered] @{
-        'method' = 'geo.getTopArtists'
-        'api_key' = $LFMConfig.APIKey
-        'format' = 'json'
-    }
-
-    #Adding key/value to hashtable based off optional parameters
-    switch ($PSBoundParameters.Keys) {
-        'Limit' {$apiParams.add('limit', $Limit)}
-        'Page' {$apiParams.add('page', $Page)}
-    }
-
-    #Building string to append to base url
-    $keyValues = $apiParams.GetEnumerator() | ForEach-Object {
-        "$($_.Name)=$($_.Value)"
-    }
-    $string = $keyValues -join '&'
-
-    $apiUrl = "$baseUrl/?$string"
-
-    $irm = Invoke-RestMethod -Uri $apiUrl
-    $hash = $irm | ConvertTo-Hashtable
-    
-    foreach ($artist in $hash.TopArtists.Artist) {
-        $artistInfo = [pscustomobject] @{
-            'Artist' = $artist.Name
-            'Id' = $artist.Mbid
-            'Url' = $artist.Url
-            'Listeners' = $artist.Listeners
-            'ImageUrl' = $artist.Image.Where({$_.Size -eq 'ExtraLarge'}).'#text'
+    begin {
+        #Default hashtable
+        $apiParams = [ordered] @{
+            'method' = 'geo.getTopArtists'
+            'api_key' = $LFMConfig.APIKey
+            'format' = 'json'
         }
 
-        $artistInfo.PSObject.TypeNames.Insert(0, 'PowerLFM.Get.TopArtists')
-        Write-Output $artistInfo
+        #Adding key/value to hashtable based off optional parameters
+        switch ($PSBoundParameters.Keys) {
+            'Limit' {$apiParams.add('limit', $Limit)}
+            'Page' {$apiParams.add('page', $Page)}
+        }
+    }
+    process {
+        switch ($PSBoundParameters.Keys) {
+            'Country' {$apiParams.add('country', $Country)}
+        }
+        #Building string to append to base url
+        $keyValues = $apiParams.GetEnumerator() | ForEach-Object {
+            "$($_.Name)=$($_.Value)"
+        }
+        $string = $keyValues -join '&'
+
+        $apiUrl = "$baseUrl/?$string"
+    }
+    end {
+        $irm = Invoke-RestMethod -Uri $apiUrl
+        $hash = $irm | ConvertTo-Hashtable
+        
+        foreach ($artist in $hash.TopArtists.Artist) {
+            $artistInfo = [pscustomobject] @{
+                'Artist' = $artist.Name
+                'Id' = $artist.Mbid
+                'Url' = $artist.Url
+                'Listeners' = [int] $artist.Listeners
+                'ImageUrl' = $artist.Image.Where({$_.Size -eq 'ExtraLarge'}).'#text'
+            }
+    
+            $artistInfo.PSObject.TypeNames.Insert(0, 'PowerLFM.Get.TopArtists')
+            Write-Output $artistInfo
+        }
     }
 }
