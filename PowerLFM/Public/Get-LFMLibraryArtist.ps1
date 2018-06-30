154f90090d36b77c36e79243588bdddebaf27bcc
John Steele <camusicjunkie@yahoo.com>
2018-06-30 10:30:56 -0600

    Added support for library.getArtists method

---
+++ PowerLFM/Public/Get-LFMLibraryArtist.ps1
@@ -0,0 +1,57 @@
+function Get-LFMLibraryArtist {
+    [CmdletBinding()]
+    [OutputType('PowerLFM.Library.Artist')]
+    param (
+        [Parameter(Mandatory,
+                   ValueFromPipelineByPropertyName)]
+        [string] $UserName,
+        
+        [string] $Limit,
+        [string] $Page
+    )
+
+    begin {
+        #Default hashtable
+        $apiParams = [ordered] @{
+            'method' = 'library.getArtists'
+            'api_key' = $LFMConfig.APIKey
+            'format' = 'json'
+        }
+
+        #Adding key/value to hashtable based off optional parameters
+        switch ($PSBoundParameters.Keys) {
+            'Limit' {$apiParams.add('limit', $Limit)}
+            'Page' {$apiParams.add('page', $Page)}
+        }
+    }
+    process {
+        switch ($PSBoundParameters.Keys) {
+            'UserName' {$apiParams.add('user', $UserName)}
+        }
+
+        #Building string to append to base url
+        $keyValues = $apiParams.GetEnumerator() | ForEach-Object {
+            "$($_.Name)=$($_.Value)"
+        }
+        $string = $keyValues -join '&'
+
+        $apiUrl = "$baseUrl/?$string"
+    }
+    end {
+        $irm = Invoke-RestMethod -Uri $apiUrl
+        $hash = $irm | ConvertTo-Hashtable
+        
+        foreach ($artist in $hash.Artists.Artist) {
+            $artistInfo = [pscustomobject] @{
+                'Artist' = $artist.Name
+                'PlayCount' = $artist.PlayCount
+                'Url' = $artist.url
+                'Id' = $artist.Mbid
+                'ImageUrl' = $artist.Image.Where({$_.Size -eq 'ExtraLarge'}).'#text'
+            }
+
+            $artistInfo.PSObject.TypeNames.Insert(0, 'PowerLFM.Library.Artist')
+            Write-Output $artistInfo
+        }
+    }
+}
