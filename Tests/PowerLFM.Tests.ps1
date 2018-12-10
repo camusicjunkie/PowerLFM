64ce828f0048aff14b6419f5440097de3dfc2fe0
John Steele <camusicjunkie@yahoo.com>
2018-12-10 15:21:18 -0700

    Adding fixed tests file

--- Tests/PowerLFM.Tests.ps1
+++ Tests/PowerLFM.Tests.ps1
@@ -1,14 +1,13 @@
 $projectRoot = Resolve-Path "$PSScriptRoot\.."
-$moduleRoot = Split-Path (Resolve-Path "$projectRoot\*.psm1")
-$moduleName = Split-Path $moduleRoot -Leaf
-$ModuleManifestName = "$moduleName.psd1"
-$ModuleManifestPath = "$PSScriptRoot\..\$ModuleManifestName"
-
+$moduleName = Split-Path $projectRoot -Leaf
+$moduleRoot = "$projectRoot\$moduleName"
+$moduleManifestName = "$moduleName.psd1"
+$moduleManifestPath = "$moduleRoot\$moduleManifestName"
 
 Describe 'Module Tests' {
     Context 'ModuleManifest' {
         It 'Passes Test-ModuleManifest' {
-            Test-ModuleManifest -Path $ModuleManifestPath | Should Not BeNullOrEmpty
+            Test-ModuleManifest -Path $moduleManifestPath | Should Not BeNullOrEmpty
             $? | Should Be $true
         }
     }
@@ -16,20 +15,20 @@ Describe 'Module Tests' {
         $scripts = Get-ChildItem $projectRoot -Include *.ps1,*.psm1,*.psd1 -Recurse
 
         # TestCases are splatted to the script so we need hashtables
-        $testCase = $scripts | Foreach-Object{@{file=$_}}         
+        $testCase = $scripts | Foreach-Object{@{file=$_}}
         It "Script <file> should be valid powershell" -TestCases $testCase {
             param($file)
-    
+
             $file.fullname | Should Exist
-    
+
             $contents = Get-Content -Path $file.fullname -ErrorAction Stop
             $errors = $null
             $null = [System.Management.Automation.PSParser]::Tokenize($contents, [ref]$errors)
-            $errors.Count | Should Be 0
+            $errors.count | Should Be 0
         }
-    
+
         It "Module '$moduleName' can import cleanly" {
-            {Import-Module (Join-Path $moduleRoot "$moduleName.psm1") -force } | Should Not Throw
+            {Import-Module (Join-Path $moduleRoot "$moduleName.psm1") -Force } | Should Not Throw
         }
     }
 }
