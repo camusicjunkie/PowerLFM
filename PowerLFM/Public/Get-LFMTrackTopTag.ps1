36dc03e5230140eba59e5527a080e97ed12ecf41
John Steele <camusicjunkie@yahoo.com>
2018-12-13 20:10:02 -0700

    Added support for track.getTopTags method

---
+++ PowerLFM/Public/Get-LFMTrackTopTag.ps1
@@ -0,0 +1,73 @@
+function Get-LFMTrackTopTag {
+    # .ExternalHelp PowerLFM.psm1-help.xml
+
+    [CmdletBinding(DefaultParameterSetName = 'track')]
+    [OutputType('PowerLFM.Track.TopTag')]
+    param (
+        [Parameter(Mandatory,
+                   ValueFromPipelineByPropertyName,
+                   ParameterSetName = 'track')]
+        [string] $Track,
+
+        [Parameter(Mandatory,
+                   ValueFromPipelineByPropertyName,
+                   ParameterSetName = 'track')]
+        [string] $Artist,
+
+        [Parameter(Mandatory,
+                   ValueFromPipelineByPropertyName,
+                   ParameterSetName = 'id')]
+        [string] $Id,
+        [switch] $AutoCorrect
+    )
+
+    begin {
+        $apiParams = [ordered] @{
+            'method' = 'track.getTopTags'
+            'api_key' = $LFMConfig.APIKey
+            'format' = 'json'
+        }
+
+        switch ($PSBoundParameters.Keys) {
+            'AutoCorrect' {$apiParams.add('autocorrect', 1)}
+        }
+    }
+    process {
+        switch ($PSCmdlet.ParameterSetName) {
+            'track' {$apiParams.add('track', $Track);
+                     $apiParams.add('artist', $Artist)}
+            'id'    {$apiParams.add('mbid', $Id)}
+        }
+
+        #Building string to append to base url
+        $keyValues = $apiParams.GetEnumerator() | ForEach-Object {
+            "$($_.Name)=$($_.Value)"
+        }
+        $string = $keyValues -join '&'
+
+        $apiUrl = "$baseUrl/?$string"
+    }
+    end {
+        $irm = Invoke-RestMethod -Uri $apiUrl
+        $hash = $irm | ConvertTo-Hashtable
+
+        $tags = foreach ($tag in $hash.TopTags.Tag) {
+            $tagInfo = [pscustomobject] @{
+                'Tag' = $tag.Name
+                'Url' = $tag.Url
+                'Match' = $tag.Count
+            }
+            $tagInfo.PSObject.TypeNames.Insert(0, 'PowerLFM.Track.Tag')
+            Write-Output $tagInfo
+        }
+
+        $TrackTagInfo = [pscustomobject] @{
+            'Track' = $hash.TopTags.'@attr'.Track
+            'Artist' = $hash.TopTags.'@attr'.Artist
+            'Tags' = $tags
+        }
+
+        $TrackTagInfo.PSObject.TypeNames.Insert(0, 'PowerLFM.Track.TopTag')
+        Write-Output $TrackTagInfo
+    }
+}
