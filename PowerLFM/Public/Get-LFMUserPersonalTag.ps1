0f16f2c5bf24193f7c7a52c32a6f02985ab4be45
John Steele <camusicjunkie@yahoo.com>
2018-12-20 18:12:47 -0800

    Added functionality for user.getPersonalTags method

---
+++ PowerLFM/Public/Get-LFMUserPersonalTag.ps1
@@ -0,0 +1,66 @@
+function Get-LFMUserPersonalTag {
+    # .ExternalHelp PowerLFM.psm1-help.xml
+
+    [CmdletBinding()]
+    #[OutputType('PowerLFM.User.PersonalTag')]
+    param (
+        [Parameter(Mandatory,
+                   ValueFromPipelineByPropertyName)]
+        [string] $UserName,
+
+        [Parameter(Mandatory,
+                   ValueFromPipelineByPropertyName)]
+        [string] $Tag,
+
+        [Parameter(Mandatory,
+                   ValueFromPipelineByPropertyName)]
+        [ValidateSet('Artist', 'Album', 'Track')]
+        [string] $TagType,
+
+        [Parameter()]
+        [ValidateRange(1,50)]
+        [string] $Limit,
+
+        [string] $Page
+    )
+
+    begin {
+        $apiParams = [ordered] @{
+            'method' = 'user.getPersonalTags'
+            'api_key' = $LFMConfig.APIKey
+            'format' = 'json'
+        }
+
+        switch ($PSBoundParameters.Keys) {
+            'Limit' {$apiParams.add('limit', $Limit)}
+            'Page' {$apiParams.add('page', $Page)}
+        }
+    }
+    process {
+        $apiParams.add('user', $UserName)
+        $apiParams.add('tag', $Tag)
+        $apiParams.add('taggingtype', $TagType.ToLower())
+
+        #Building string to append to base url
+        $keyValues = $apiParams.GetEnumerator() | ForEach-Object {
+            "$($_.Name)=$($_.Value)"
+        }
+        $string = $keyValues -join '&'
+
+        $apiUrl = "$baseUrl/?$string"
+    }
+    end {
+        $irm = Invoke-RestMethod -Uri $apiUrl
+
+        foreach ($userTag in $irm.Taggings.Artists.Artist) {
+            $userTagInfo = [pscustomobject] @{
+                'Artist' = $userTag.name
+                'Id' = $userTag.mbid
+                'Url' = $userTag.url
+            }
+
+            $userTagInfo.PSObject.TypeNames.Insert(0, 'PowerLFM.User.PersonalTag')
+            Write-Output $userTagInfo
+        }
+    }
+}
