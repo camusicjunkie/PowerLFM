b7b8c00022add29f796c9bc88ae2d230952d6075
John Steele <camusicjunkie@yahoo.com>
2019-05-29 20:10:44 +0200

    removed supportsshouldprocess support on private function

--- PowerLFM/Private/New-LFMAlbumSignature.ps1
+++ PowerLFM/Private/New-LFMAlbumSignature.ps1
@@ -1,6 +1,5 @@
 function New-LFMAlbumSignature {
-    [CmdletBinding(SupportsShouldProcess,
-                   ConfirmImpact = 'Medium')]
+    [CmdletBinding()]
     [OutputType('System.String')]
     param (
         [Parameter(Mandatory)]
@@ -42,9 +41,7 @@ function New-LFMAlbumSignature {
 
         $string = $keyValues -join ''
 
-        if ($PSCmdlet.ShouldProcess('Shared secret', 'Creating album signature')) {
-            Get-Md5Hash -String "$string$($LFMConfig.SharedSecret)"
-        }
+        Get-Md5Hash -String "$string$($LFMConfig.SharedSecret)"
         Write-Verbose "$string$($LFMConfig.SharedSecret)"
     }
     catch {
