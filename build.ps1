38b756206b003bbcc9eff4f6ce3d3fcefb8fe178
John Steele <camusicjunkie@yahoo.com>
2019-05-29 22:35:33 +0200

    Added APPVEYOR check

--- build.ps1
+++ build.ps1
@@ -10,5 +10,9 @@ if (-not (Get-Module -Name psake -ListAvailable)) {Install-Module -Name psake -S
 if (-not (Get-Module -Name PSDeploy -ListAvailable)) {Install-Module -Name PSDeploy -Scope CurrentUser -Force}
 if (-not (Get-Module -Name PSScriptAnalyzer -ListAvailable)) {Install-Module -Name PSScriptAnalyzer -Scope CurrentUser -Force}
 
+if ($env:APPVEYOR) {
+    Write-Output 'Inside APPVEYOR if statement'
+}
+
 Invoke-Psake -buildFile "$PSScriptRoot\psake.ps1" -taskList $Task -Verbose:$VerbosePreference
 exit ([int] (-not $psake.build_success))
