6f35eb40ddebd5fdb651d1bd2aba28f871977f75
John Steele <camusicjunkie@yahoo.com>
2019-06-10 21:17:38 +0200

    moved module test to new folder

---
+++ Tests/Module/PowerLFM.Tests.ps1
@@ -0,0 +1,35 @@
+$projectRoot = Resolve-Path "$PSScriptRoot\..\.."
+$moduleName = Split-Path $projectRoot -Leaf
+$moduleRoot = "$projectRoot\$moduleName"
+$moduleManifestName = "$moduleName.psd1"
+$moduleManifestPath = "$moduleRoot\$moduleManifestName"
+
+Describe 'Module Tests' {
+    It 'Module manifest should exist and be valid' {
+        Test-ModuleManifest -Path $moduleManifestPath | Should -Not -BeNullOrEmpty
+        $? | Should Be $true
+    }
+
+    It "Module '$moduleName' can import cleanly" {
+        {Import-Module (Join-Path $moduleRoot "$moduleName.psm1") -Force} | Should -Not -Throw
+    }
+
+    Context 'General project validation' {
+        $scripts = Get-ChildItem $projectRoot -Include *.ps1, *.psm1, *.psd1 -Recurse
+
+        $testCases = $scripts | Foreach-Object { @{file = $_.Name} }
+
+        It "Script <file> should be valid powershell" -TestCases $testCases {
+            param($file)
+
+            $fullName = (Get-ChildItem -Path $projectRoot -Include $file -Recurse -File).FullName
+            $fullname | Should Exist
+
+            $contents = Get-Content -Path $fullname -ErrorAction Stop
+            $errors = $null
+            $null = [System.Management.Automation.PSParser]::Tokenize($contents, [ref]$errors)
+            $errors.count | Should Be 0
+        }
+
+    }
+}
