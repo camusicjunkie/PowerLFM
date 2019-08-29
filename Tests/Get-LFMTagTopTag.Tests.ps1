b8d493464fd3b03bff8fe55e47f722f992dd0b5f
John Steele <33888807+camusicjunkie@users.noreply.github.com>
2019-08-28 19:47:05 -0700

    added unit test

--- Tests/Get-LFMTagTopTag.Tests.ps1
+++ Tests/Get-LFMTagTopTag.Tests.ps1
@@ -25,22 +25,74 @@ Describe 'Get-LFMTagTopTag: Interface' -Tag Interface {
 
 InModuleScope PowerLFM {
 
+    $mocks = Get-Content -Path $PSScriptRoot\..\config\mocks.json | ConvertFrom-Json
+    $contextMock = $mocks.'Get-LFMTagTopTag'.TagTopTag
+
     Describe 'Get-LFMTagTopTag: Unit' -Tag Unit {
 
-        Context 'Input' {
-
-        }
+        Mock Invoke-RestMethod
 
         Context 'Execution' {
 
+            Mock Foreach-Object
+
+            It 'Should call Foreach-Object 3 times building url' {
+
+                Get-LFMTagTopTag
+
+                $amParams = @{
+                    CommandName = 'Foreach-Object'
+                    Exactly = $true
+                    Times = 3
+                    Scope = 'It'
+                }
+                Assert-MockCalled @amParams
+            }
         }
 
         Context 'Output' {
 
+            Mock Invoke-RestMethod {$contextMock}
+
+            BeforeEach {
+                $script:output = Get-LFMTagTopTag
+            }
+
+            It "Tag first top tag should have name of $($contextMock.TopTags.Tag[0].Name)" {
+                $output[0].Tag | Should -Be $contextMock.TopTags.Tag[0].Name
+            }
+
+            It "Tag first top tag should have reach of $($contextMock.TopTags.Tag[0].Reach)" {
+                $output[0].Reach | Should -Be $contextMock.TopTags.Tag[0].Reach
+            }
+
+            It "Tag first top tag should have count with a value of $($contextMock.TopTags.Tag[0].Count)" {
+                $output[0].Count | Should -Be $contextMock.TopTags.Tag[0].Count
+            }
+
+            It "Tag second top tag should have count with a value of $($contextMock.TopTags.Tag[1].Count)" {
+                $output[1].Count | Should -Be $contextMock.TopTags.Tag[1].Count
+            }
+
+            It "Tag second top tag should have reach with a value of $($contextMock.TopTags.Tag[1].Reach)" {
+                $output[1].Reach | Should -Be $contextMock.TopTags.Tag[1].Reach
+            }
+
+            It 'Tag should have two top tags' {
+                $output.Tag | Should -HaveCount 2
+            }
+
+            It 'Tag should not have more than two top tags' {
+                $output.Tag | Should -Not -BeNullOrEmpty
+                $output.Tag | Should -Not -HaveCount 3
+            }
         }
     }
 }
 
 Describe 'Get-LFMTagTopTag: Integration' -Tag Integration {
 
+    It "Integration test" {
+        Set-ItResult -Skipped -Because 'the integration tests will be set up later'
+    }
 }
