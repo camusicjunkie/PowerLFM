fdb6da9d8558e1d44ab20ec222bc577b06ab3a79
John Steele <33888807+camusicjunkie@users.noreply.github.com>
2019-08-29 09:50:34 -0700

    added unit tests

--- Tests/Get-LFMTagTopTrack.Tests.ps1
+++ Tests/Get-LFMTagTopTrack.Tests.ps1
@@ -126,22 +126,118 @@ Describe 'Get-LFMTagTopTrack: Interface' -Tag Interface {
 
 InModuleScope PowerLFM {
 
+    $mocks = Get-Content -Path $PSScriptRoot\..\config\mocks.json | ConvertFrom-Json
+    $contextMock = $mocks.'Get-LFMTagTopTrack'.TagTopTrack
+
     Describe 'Get-LFMTagTopTrack: Unit' -Tag Unit {
+
+        Mock Invoke-RestMethod
 
         Context 'Input' {
 
+            It "Should throw when Tag is null" {
+                {Get-LFMTagTopTrack -Tag $null} | Should -Throw
+            }
         }
 
         Context 'Execution' {
 
+            Mock Foreach-Object
+
+            $testCases = @(
+                @{
+                    times = 4
+                    gtttParams = @{
+                        Tag = 'Tag'
+                    }
+                }
+                @{
+                    times = 5
+                    gtttParams = @{
+                        Tag = 'Tag'
+                        Limit = '5'
+                    }
+                }
+                @{
+                    times = 6
+                    gtttParams = @{
+                        Tag = 'Tag'
+                        Limit = '5'
+                        Page = '1'
+                    }
+                }
+            )
+
+            It 'Should call Foreach-Object <times> times building url' -TestCases $testCases {
+                param ($times, $gtttParams)
+
+                Get-LFMTagTopTrack @gtttParams
+
+                $amParams = @{
+                    CommandName = 'Foreach-Object'
+                    Exactly = $true
+                    Times = $times
+                    Scope = 'It'
+                }
+                Assert-MockCalled @amParams
+            }
         }
 
         Context 'Output' {
 
+            Mock Invoke-RestMethod {$contextMock}
+
+            BeforeEach {
+                $script:output = Get-LFMTagTopTrack -Tag Tag
+            }
+
+            It "Tag first top track should have name of $($contextMock.Tracks.Track[0].Name)" {
+                $output[0].Track | Should -Be $contextMock.Tracks.Track[0].Name
+            }
+
+            It "Tag first top track should have artist name of $($contextMock.Tracks.Track[0].Artist.Name)" {
+                $output[0].Artist | Should -Be $contextMock.Tracks.Track[0].Artist.Name
+            }
+
+            It "Tag first top track should have url of $($contextMock.Tracks.Track[0].Url)" {
+                $output[0].TrackUrl | Should -Be $contextMock.Tracks.Track[0].Url
+            }
+
+            It "Tag first top track should have rank with a value of $($contextMock.Tracks.Track[0].'@attr'.Rank)" {
+                $output[0].Rank | Should -Be $contextMock.Tracks.Track[0].'@attr'.Rank
+            }
+
+            It "Tag second top track should have duration with a value of $($contextMock.Tracks.Track[1].Duration)" {
+                $output[1].Duration | Should -Be $contextMock.Tracks.Track[1].Duration
+            }
+
+            It "Tag second top track should have url of $($contextMock.Tracks.Track[1].Url)" {
+                $output[1].TrackUrl | Should -Be $contextMock.Tracks.Track[1].Url
+            }
+
+            It "Tag second top track should have artist id with a value of $($contextMock.Tracks.Track[1].Artist.Mbid)" {
+                $output[1].ArtistId | Should -Be $contextMock.Tracks.Track[1].Artist.Mbid
+            }
+
+            It "Tag second top track should have artist url of $($contextMock.Tracks.Track[1].Artist.Url)" {
+                $output[1].ArtistUrl | Should -Be $contextMock.Tracks.Track[1].Artist.Url
+            }
+
+            It 'Tag should have two top tracks' {
+                $output.Track | Should -HaveCount 2
+            }
+
+            It 'Tag should not have more than two top tracks' {
+                $output.Track | Should -Not -BeNullOrEmpty
+                $output.Track | Should -Not -HaveCount 3
+            }
         }
     }
 }
 
 Describe 'Get-LFMTagTopTrack: Integration' -Tag Integration {
 
+    It "Integration test" {
+        Set-ItResult -Skipped -Because 'the integration tests will be set up later'
+    }
 }
