c15db11f291f4c00e329924ca22cfb4623dfac5a
John Steele <33888807+camusicjunkie@users.noreply.github.com>
2019-09-07 02:08:34 -0700

    added unit tests

--- Tests/Get-LFMTrackInfo.Tests.ps1
+++ Tests/Get-LFMTrackInfo.Tests.ps1
@@ -271,22 +271,143 @@ Describe 'Get-LFMTrackInfo: Interface' -Tag Interface {
 
 InModuleScope PowerLFM {
 
+    $mocks = Get-Content -Path $PSScriptRoot\..\config\mocks.json | ConvertFrom-Json
+    $contextMock = $mocks.'Get-LFMTrackInfo'.TrackInfo
+
     Describe 'Get-LFMTrackInfo: Unit' -Tag Unit {
+
+        Mock Invoke-RestMethod
 
         Context 'Input' {
 
+            It "Should throw when Track is null" {
+                {Get-LFMTrackInfo -Track $null} | Should -Throw
+            }
         }
 
         Context 'Execution' {
 
+            Mock Foreach-Object
+
+            $testCases = @(
+                @{
+                    set = 'track'
+                    times = 5
+                    gtiParams = @{
+                        Track = 'Track'
+                        Artist = 'Artist'
+                    }
+                }
+                @{
+                    set = 'track'
+                    times = 6
+                    gtiParams = @{
+                        Track = 'Track'
+                        Artist = 'Artist'
+                        UserName = 'UserName'
+                    }
+                }
+                @{
+                    set = 'track'
+                    times = 7
+                    gtiParams = @{
+                        Track = 'Track'
+                        Artist = 'Artist'
+                        UserName = 'UserName'
+                        AutoCorrect = $true
+                    }
+                }
+                @{
+                    set = 'id'
+                    times = 4
+                    gtiParams = @{
+                        Id = (New-Guid)
+                    }
+                }
+            )
+
+            It 'Should call Foreach-Object <times> times building url in <set> parameter set' -TestCases $testCases {
+                param ($times, $gtiParams)
+
+                Get-LFMTrackInfo @gtiParams
+
+                $amParams = @{
+                    CommandName = 'Foreach-Object'
+                    Exactly = $true
+                    Times = $times
+                    Scope = 'It'
+                }
+                Assert-MockCalled @amParams
+            }
         }
 
         Context 'Output' {
 
+            Mock Invoke-RestMethod {$contextMock}
+
+            BeforeEach {
+                $script:output = Get-LFMTrackInfo -Track Track -Artist Artist
+            }
+
+            It "Track should have name of $($contextMock.Track.Name)" {
+                $output.Track | Should -Be $contextMock.Track.Name
+            }
+
+            It "Track should have artist name of $($contextMock.Track.Artist.Name)" {
+                $output.Artist | Should -Be $contextMock.Track.Artist.Name
+            }
+
+            It "Track should have album name of $($contextMock.Track.Album.Title)" {
+                $output.Album | Should -Be $contextMock.Track.Album.Title
+            }
+
+            It "Track should have url of $($contextMock.Track.Url)" {
+                $output.Url | Should -Be $contextMock.Track.Url
+            }
+
+            It "Track should have listeners with a value of $($contextMock.Track.Listeners)" {
+                $output.Listeners | Should -BeOfType [int]
+                $output.Listeners | Should -Be $contextMock.Track.Listeners
+            }
+
+            It "Track should have playcount with a value of $($contextMock.Track.Playcount)" {
+                $output.Playcount | Should -BeOfType [int]
+                $output.Playcount | Should -Be $contextMock.Track.Playcount
+            }
+
+            It "Track first tag should have name of $($contextMock.Track.TopTags.Tag[0].Name)" {
+                $output.Tags[0].Tag | Should -Be $contextMock.Track.TopTags.Tag[0].Name
+            }
+
+            It "Track second tag should have a url of $($contextMock.Track.TopTags.Tag[1].Url)" {
+                $output.Tags[1].Url | Should -Be $contextMock.Track.TopTags.Tag[1].Url
+            }
+
+            It 'Track should have two tags' {
+                $output.Tags | Should -HaveCount 2
+            }
+
+            It 'Track should not have more than two tags' {
+                $output.Tags | Should -Not -BeNullOrEmpty
+                $output.Tags | Should -Not -HaveCount 3
+            }
+
+            It "Track should have a user play count of $($contextMock.Track.UserPlayCount)" {
+                $output = Get-LFMTrackInfo -Track Track -Artist Artist -UserName camusicjunkie
+                $output.UserPlayCount | Should -Be $contextMock.Track.UserPlayCount
+            }
+
+            It "Track should have two tags when id parameter is used" {
+                $output = Get-LFMTrackInfo -Id 1
+                $output.Tags | Should -HaveCount 2
+            }
         }
     }
 }
 
 Describe 'Get-LFMTrackInfo: Integration' -Tag Integration {
 
+    It "Integration test" {
+        Set-ItResult -Skipped -Because 'the integration tests will be set up later'
+    }
 }
