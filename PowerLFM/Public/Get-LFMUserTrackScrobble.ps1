9b4ca6a60dcf8fe3213480f0573c3a8de3140888
John Steele <33888807+camusicjunkie@users.noreply.github.com>
2019-09-28 11:24:17 -0700

    added function for user.getTrackScrobbles method

---
+++ PowerLFM/Public/Get-LFMUserTrackScrobble.ps1
@@ -0,0 +1,73 @@
+function Get-LFMUserTrackScrobble {
+    <# # .ExternalHelp PowerLFM.psm1-help.xml#>
+
+    [CmdletBinding()]
+    [OutputType('PowerLFM.User.TrackScrobble')]
+    param (
+        [Parameter(Mandatory,
+                   ValueFromPipelineByPropertyName)]
+        [ValidateNotNullOrEmpty()]
+        [string] $Track,
+
+        [Parameter(Mandatory,
+                   ValueFromPipelineByPropertyName)]
+        [ValidateNotNullOrEmpty()]
+        [string] $Artist,
+
+        [Parameter(ValueFromPipelineByPropertyName)]
+        [ValidateNotNullOrEmpty()]
+        [string] $UserName,
+
+        [Parameter()]
+        [ValidateRange(1,50)]
+        [string] $Limit,
+
+        [string] $Page
+    )
+
+    begin {
+        $apiParams = @{
+            'method' = 'user.getTrackScrobbles'
+            'api_key' = $LFMConfig.APIKey
+            'sk' = $LFMConfig.SessionKey
+            'format' = 'json'
+        }
+
+        switch ($PSBoundParameters.Keys) {
+            'Limit' {$apiParams.Add('limit', $Limit)}
+            'Page' {$apiParams.Add('page', $Page)}
+        }
+    }
+    process {
+        $apiParams.Add('track', $Track)
+        $apiParams.Add('artist', $Artist)
+
+        if ($PSBoundParameters.ContainsKey('UserName')) {
+            $apiParams.Remove('sk')
+            $apiParams.Add('user', $UserName)
+        }
+
+        #Building string to append to base url
+        $keyValues = $apiParams.GetEnumerator() | ForEach-Object {
+            "$($_.Name)=$($_.Value)"
+        }
+        $string = $keyValues -join '&'
+
+        $apiUrl = "$baseUrl/?$string"
+    }
+    end {
+        $irm = Invoke-LFMApiUri -Uri $apiUrl
+        if ($irm.Error) {Write-Output $irm; return}
+
+        foreach ($scrobble in $irm.Trackscrobbles.Track) {
+            [PSCustomObject] @{
+                'Track' = $scrobble.Name
+                'TrackId' = $scrobble.Mbid
+                'TrackUrl' = $scrobble.Url
+                'Artist' = $scrobble.Artist.'#text'
+                'Album' = $scrobble.Album.'#text'
+                'Date' = ConvertFrom-UnixTime -UnixTime $scrobble.Date.Uts
+            }
+        }
+    }
+}
