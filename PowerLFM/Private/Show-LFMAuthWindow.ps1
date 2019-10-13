f4aeb3fdf2ff35ec105000f52d73e9c72df69da3
John Steele <33888807+camusicjunkie@users.noreply.github.com>
2019-10-13 00:34:41 -0700

    refactor token request

---
+++ PowerLFM/Private/Show-LFMAuthWindow.ps1
@@ -0,0 +1,40 @@
+Function Show-LFMAuthWindow {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory)]
+        [uri] $Url
+    )
+
+    Add-Type -AssemblyName System.Windows.Forms
+
+    $form = New-Object -TypeName System.Windows.Forms.Form -Property @{
+        Width = 515
+        Height = 638
+        StartPosition = 1
+        FormBorderStyle = 'Fixed3D'
+        MinimizeBox = $false
+        MaximizeBox = $false
+        ShowIcon = $false
+    }
+
+    $web = New-Object -TypeName System.Windows.Forms.WebBrowser -Property @{
+        Width = 500
+        Height = 600
+        ScrollBarsEnabled = $false
+        Url = $Url
+    }
+
+    $docComp = {
+        $uri = $web.Url.AbsoluteUri
+        if ($uri -cmatch '/None') {
+            $form.Close()
+        }
+    }
+
+    $web.ScriptErrorsSuppressed = $true
+    $web.Add_DocumentCompleted($docComp)
+
+    $form.Controls.Add($web)
+    $form.Activate()
+    $null = $form.ShowDialog()
+}
