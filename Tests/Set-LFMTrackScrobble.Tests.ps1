32d7c331373b4e185baa4e83b1e580ce99184916
John Steele <33888807+camusicjunkie@users.noreply.github.com>
2019-11-06 00:44:01 -0700

    add test for Set-LFMTrackScrobble

---
+++ Tests/Set-LFMTrackScrobble.Tests.ps1
@@ -0,0 +1,308 @@
+Remove-Module -Name PowerLFM -ErrorAction Ignore
+Import-Module -Name $PSScriptRoot\..\PowerLFM\PowerLFM.psd1
+
+Describe 'Set-LFMTrackScrobble: Interface' -Tag Interface {
+
+    BeforeAll {
+        $script:command = (Get-Command -Name 'Set-LFMTrackScrobble')
+    }
+
+    It 'CmdletBinding should be declared' {
+        $command.CmdletBinding | Should -BeTrue
+    }
+
+    Context 'ParameterSetName __AllParameterSets' {
+
+        It 'Should have a parameter set of __AllParameterSets' {
+            $command.ParameterSets.Name -contains '__AllParameterSets' | Should -BeTrue
+        }
+
+        $parameterSet = $command.ParameterSets | Where-Object Name -eq __AllParameterSets
+
+        Context 'Parameter [Artist] attribute validation' {
+
+            $parameter = $parameterSet.Parameters | Where-Object Name -eq Artist
+
+            It 'Should not be null or empty' {
+                $parameter | Should -Not -BeNullOrEmpty
+            }
+
+            It "Should be of type System.String" {
+                $parameter.ParameterType.ToString() | Should -Be System.String
+            }
+
+            It 'Mandatory should be set to True' {
+                $parameter.IsMandatory | Should -BeTrue
+            }
+
+            It 'ValueFromPipeline should be set to False' {
+                $parameter.ValueFromPipeline | Should -BeFalse
+            }
+
+            It 'ValueFromPipelineByPropertyName should be set to True' {
+                $parameter.ValueFromPipelineByPropertyName | Should -BeTrue
+            }
+
+            It 'ValueFromRemainingArguments should be set to False' {
+                $parameter.ValueFromRemainingArguments | Should -BeFalse
+            }
+
+            It "Should have a position of 0" {
+                $parameter.Position | Should -Be 0
+            }
+        }
+
+        Context 'Parameter [Track] attribute validation' {
+
+            $parameter = $parameterSet.Parameters | Where-Object Name -eq Track
+
+            It 'Should not be null or empty' {
+                $parameter | Should -Not -BeNullOrEmpty
+            }
+
+            It "Should be of type System.String" {
+                $parameter.ParameterType.ToString() | Should -Be System.String
+            }
+
+            It 'Mandatory should be set to True' {
+                $parameter.IsMandatory | Should -BeTrue
+            }
+
+            It 'ValueFromPipeline should be set to False' {
+                $parameter.ValueFromPipeline | Should -BeFalse
+            }
+
+            It 'ValueFromPipelineByPropertyName should be set to True' {
+                $parameter.ValueFromPipelineByPropertyName | Should -BeTrue
+            }
+
+            It 'ValueFromRemainingArguments should be set to False' {
+                $parameter.ValueFromRemainingArguments | Should -BeFalse
+            }
+
+            It "Should have a position of 1" {
+                $parameter.Position | Should -Be 1
+            }
+        }
+
+        Context 'Parameter [Timestamp] attribute validation' {
+
+            $parameter = $parameterSet.Parameters | Where-Object Name -eq Timestamp
+
+            It 'Should not be null or empty' {
+                $parameter | Should -Not -BeNullOrEmpty
+            }
+
+            It "Should be of type System.DateTime" {
+                $parameter.ParameterType.ToString() | Should -Be System.DateTime
+            }
+
+            It 'Mandatory should be set to True' {
+                $parameter.IsMandatory | Should -BeTrue
+            }
+
+            It 'ValueFromPipeline should be set to False' {
+                $parameter.ValueFromPipeline | Should -BeFalse
+            }
+
+            It 'ValueFromPipelineByPropertyName should be set to True' {
+                $parameter.ValueFromPipelineByPropertyName | Should -BeTrue
+            }
+
+            It 'ValueFromRemainingArguments should be set to False' {
+                $parameter.ValueFromRemainingArguments | Should -BeFalse
+            }
+
+            It "Should have a position of 2" {
+                $parameter.Position | Should -Be 2
+            }
+        }
+
+        Context 'Parameter [Album] attribute validation' {
+
+            $parameter = $parameterSet.Parameters | Where-Object Name -eq Album
+
+            It 'Should not be null or empty' {
+                $parameter | Should -Not -BeNullOrEmpty
+            }
+
+            It "Should be of type System.String" {
+                $parameter.ParameterType.ToString() | Should -Be System.String
+            }
+
+            It 'Mandatory should be set to False' {
+                $parameter.IsMandatory | Should -BeFalse
+            }
+
+            It 'ValueFromPipeline should be set to False' {
+                $parameter.ValueFromPipeline | Should -BeFalse
+            }
+
+            It 'ValueFromPipelineByPropertyName should be set to True' {
+                $parameter.ValueFromPipelineByPropertyName | Should -BeTrue
+            }
+
+            It 'ValueFromRemainingArguments should be set to False' {
+                $parameter.ValueFromRemainingArguments | Should -BeFalse
+            }
+
+            It "Should have a position of 3" {
+                $parameter.Position | Should -Be 3
+            }
+        }
+
+        Context 'Parameter [Id] attribute validation' {
+
+            $parameter = $parameterSet.Parameters | Where-Object Name -eq Id
+
+            It 'Should not be null or empty' {
+                $parameter | Should -Not -BeNullOrEmpty
+            }
+
+            It "Should be of type System.Guid" {
+                $parameter.ParameterType.ToString() | Should -Be System.Guid
+            }
+
+            It 'Mandatory should be set to False' {
+                $parameter.IsMandatory | Should -BeFalse
+            }
+
+            It 'ValueFromPipeline should be set to False' {
+                $parameter.ValueFromPipeline | Should -BeFalse
+            }
+
+            It 'ValueFromPipelineByPropertyName should be set to True' {
+                $parameter.ValueFromPipelineByPropertyName | Should -BeTrue
+            }
+
+            It 'ValueFromRemainingArguments should be set to False' {
+                $parameter.ValueFromRemainingArguments | Should -BeFalse
+            }
+
+            It "Should have a position of 4" {
+                $parameter.Position | Should -Be 4
+            }
+        }
+
+        Context 'Parameter [TrackNumber] attribute validation' {
+
+            $parameter = $parameterSet.Parameters | Where-Object Name -eq TrackNumber
+
+            It 'Should not be null or empty' {
+                $parameter | Should -Not -BeNullOrEmpty
+            }
+
+            It "Should be of type System.Int32" {
+                $parameter.ParameterType.ToString() | Should -Be System.Int32
+            }
+
+            It 'Mandatory should be set to False' {
+                $parameter.IsMandatory | Should -BeFalse
+            }
+
+            It 'ValueFromPipeline should be set to False' {
+                $parameter.ValueFromPipeline | Should -BeFalse
+            }
+
+            It 'ValueFromPipelineByPropertyName should be set to True' {
+                $parameter.ValueFromPipelineByPropertyName | Should -BeTrue
+            }
+
+            It 'ValueFromRemainingArguments should be set to False' {
+                $parameter.ValueFromRemainingArguments | Should -BeFalse
+            }
+
+            It "Should have a position of 5" {
+                $parameter.Position | Should -Be 5
+            }
+        }
+
+        Context 'Parameter [Duration] attribute validation' {
+
+            $parameter = $parameterSet.Parameters | Where-Object Name -eq Duration
+
+            It 'Should not be null or empty' {
+                $parameter | Should -Not -BeNullOrEmpty
+            }
+
+            It "Should be of type System.Int32" {
+                $parameter.ParameterType.ToString() | Should -Be System.Int32
+            }
+
+            It 'Mandatory should be set to False' {
+                $parameter.IsMandatory | Should -BeFalse
+            }
+
+            It 'ValueFromPipeline should be set to False' {
+                $parameter.ValueFromPipeline | Should -BeFalse
+            }
+
+            It 'ValueFromPipelineByPropertyName should be set to True' {
+                $parameter.ValueFromPipelineByPropertyName | Should -BeTrue
+            }
+
+            It 'ValueFromRemainingArguments should be set to False' {
+                $parameter.ValueFromRemainingArguments | Should -BeFalse
+            }
+
+            It "Should have a position of 6" {
+                $parameter.Position | Should -Be 6
+            }
+        }
+
+        Context 'Parameter [PassThru] attribute validation' {
+
+            $parameter = $parameterSet.Parameters | Where-Object Name -eq PassThru
+
+            It 'Should not be null or empty' {
+                $parameter | Should -Not -BeNullOrEmpty
+            }
+
+            It "Should be of type System.Management.Automation.SwitchParameter" {
+                $parameter.ParameterType.ToString() | Should -Be System.Management.Automation.SwitchParameter
+            }
+
+            It 'Mandatory should be set to False' {
+                $parameter.IsMandatory | Should -BeFalse
+            }
+
+            It 'ValueFromPipeline should be set to False' {
+                $parameter.ValueFromPipeline | Should -BeFalse
+            }
+
+            It 'ValueFromPipelineByPropertyName should be set to False' {
+                $parameter.ValueFromPipelineByPropertyName | Should -BeFalse
+            }
+
+            It 'ValueFromRemainingArguments should be set to False' {
+                $parameter.ValueFromRemainingArguments | Should -BeFalse
+            }
+
+            It "Should have a position of -2147483648" {
+                $parameter.Position | Should -Be -2147483648
+            }
+        }
+    }
+}
+
+InModuleScope PowerLFM {
+
+    Describe 'Set-LFMTrackScrobble: Unit' -Tag Unit {
+
+        Context 'Input' {
+
+        }
+
+        Context 'Execution' {
+
+        }
+
+        Context 'Output' {
+
+        }
+    }
+}
+
+Describe 'Set-LFMTrackScrobble: Integration' -Tag Integration {
+
+}
