b4808dfe9c63bd822c7644e3cf65df0ff1ac6f95
John Steele <33888807+camusicjunkie@users.noreply.github.com>
2019-11-08 00:46:55 -0700

    formatting

--- PowerLFM/Private/Remove-CommonParameter.ps1
+++ PowerLFM/Private/Remove-CommonParameter.ps1
@@ -8,8 +8,8 @@ function Remove-CommonParameter {
     $commonParams = [System.Management.Automation.PSCmdlet]::CommonParameters +
                     [System.Management.Automation.PSCmdlet]::OptionalCommonParameters
 
-    $keysToRemove = $InputObject.Keys.Where({$_ -in $commonParams})
-    $null = $keysToRemove | ForEach-Object {$InputObject.Remove($_)}
+    $keysToRemove = $InputObject.Keys.Where({ $_ -in $commonParams })
+    $null = $keysToRemove | ForEach-Object { $InputObject.Remove($_) }
 
     Write-Output $InputObject
 }
