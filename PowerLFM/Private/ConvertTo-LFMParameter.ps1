758605260b6baf72cfe356764c7906b2c5ea61c6
John Steele <33888807+camusicjunkie@users.noreply.github.com>
2019-11-08 00:45:12 -0700

    add more parameters to lookup table

--- PowerLFM/Private/ConvertTo-LFMParameter.ps1
+++ PowerLFM/Private/ConvertTo-LFMParameter.ps1
@@ -16,18 +16,22 @@ function ConvertTo-LFMParameter {
         'Limit'       = 'limit'
         'Page'        = 'page'
         'StartDate'   = 'from'
-        'Tag'         = 'tag'
+        'Tag'         = 'tags'
         'TagType'     = 'taggingtype'
         'TimePeriod'  = 'period'
         'Track'       = 'track'
         'UserName'    = 'user'
+        'Token'       = 'token'
+        'ApiKey'      = 'api_key'
     }
 
     $callingCommand = (Get-PSCallStack)[-2].Command
     if ($callingCommand -like 'Get-LFM*Info') {$lfmParameter['Username'] = 'username'}
-    if ($callingCommand -like 'Remove-LFM*Tag') {$lfmParameter['Tag'] = 'tags'}
+    if ($callingCommand -like 'Remove-LFM*Tag') {$lfmParameter['Tag'] = 'tag'}
 
-    If ($InputObject.ContainsKey('PassThru')) {$InputObject.Remove('PassThru')}
+    $null = if ($InputObject.ContainsKey('Method')) {$InputObject.Remove('Method')}
+    $null = If ($InputObject.ContainsKey('SharedSecret')) {$InputObject.Remove('SharedSecret')}
+    $null = If ($InputObject.ContainsKey('PassThru')) {$InputObject.Remove('PassThru')}
 
     $hash = @{}
     foreach ($key in $InputObject.Keys) {
