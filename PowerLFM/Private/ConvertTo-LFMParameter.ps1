dfe365bf3b02cbcc0e193d1584e04e55701f5903
John Steele <33888807+camusicjunkie@users.noreply.github.com>
2019-11-10 23:26:20 -0800

    lookup table added for time period

--- PowerLFM/Private/ConvertTo-LFMParameter.ps1
+++ PowerLFM/Private/ConvertTo-LFMParameter.ps1
@@ -19,10 +19,20 @@ function ConvertTo-LFMParameter {
         'Tag'         = 'tags'
         'TagType'     = 'taggingtype'
         'TimePeriod'  = 'period'
+        'Timestamp'   = 'timestamp'
         'Track'       = 'track'
         'UserName'    = 'user'
         'Token'       = 'token'
         'ApiKey'      = 'api_key'
+    }
+
+    $period = @{
+        'Overall' = 'overall'
+        '7 Days' = '7days'
+        '1 Month' = '1month'
+        '3 Months' = '3month'
+        '6 Months' = '6month'
+        '1 Year' = '12month'
     }
 
     $callingCommand = (Get-PSCallStack)[-2].Command
@@ -32,6 +42,7 @@ function ConvertTo-LFMParameter {
     if ($InputObject.ContainsKey('Timestamp')) { $InputObject['Timestamp'] = (ConvertTo-UnixTime -Date $Timestamp) }
     if ($InputObject.ContainsKey('StartDate')) { $InputObject['StartDate'] = (ConvertTo-UnixTime -Date $StartDate) }
     if ($InputObject.ContainsKey('EndDate')) { $InputObject['EndDate'] = (ConvertTo-UnixTime -Date $EndDate) }
+    if ($InputObject.ContainsKey('TimePeriod')) { $InputObject['TimePeriod'] = $period[$InputObject['TimePeriod']] }
 
     $null = if ($InputObject.ContainsKey('Method')) { $InputObject.Remove('Method') }
     $null = If ($InputObject.ContainsKey('SharedSecret')) { $InputObject.Remove('SharedSecret') }
