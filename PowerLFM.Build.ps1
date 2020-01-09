64ed8c190cbee0c27aebcb193a1b6ef8d1e4d146
John Steele <33888807+camusicjunkie@users.noreply.github.com>
2020-01-09 12:01:54 -0700

    updated invoke-build tasks

--- PowerLFM.Build.ps1
+++ PowerLFM.Build.ps1
@@ -1,19 +1,15 @@
 #requires -Modules InvokeBuild
 
 [CmdletBinding()]
-[System.Diagnostics.CodeAnalysis.SuppressMessage('PSAvoidUsingWriteHost', '')]
-[System.Diagnostics.CodeAnalysis.SuppressMessage('PSAvoidUsingEmptyCatchBlock', '')]
 param(
-    [Parameter(Position = 0)]
-    $Tasks,
     [string[]]
     $Tag,
+
     [string]
     $PSGalleryAPIKey,
+
     [string]
-    $GithubAccessToken,
-    [switch]
-    $ResolveDependency
+    $GithubAccessToken
 )
 
 $WarningPreference = "Continue"
@@ -24,33 +20,22 @@ if ($PSBoundParameters.ContainsKey('Debug')) {
     $DebugPreference = "Continue"
 }
 
-Import-Module "$PSScriptRoot\BuildTools.psm1" -Force
-
-if ($ResolveDependency) {
-    Write-Host "Resolving Dependencies... [this can take a moment]"
-    $rsParams = @{}
-    if ($PSBoundParameters.ContainsKey('Verbose')) {
-        $rsParams.Add('Verbose', $Verbose)
-    }
-    Resolve-Dependency @rsParams
-}
-
-if ($MyInvocation.ScriptName -notlike '*Invoke-Build.ps1') {
-    if ($PSBoundParameters.ContainsKey('ResolveDependency')) {
-        Write-Verbose "Dependency already resolved. Skipping"
-        $null = $PSBoundParameters.Remove('ResolveDependency')
-    }
-
-    Invoke-Build $Tasks $MyInvocation.MyCommand.Path @PSBoundParameters
-    return
-}
-
 Enter-Build {
-    Write-Build Yellow 'Build: Inside Enter-Build'
-    Write-Host 'Host: Inside Enter-Build' -ForegroundColor Yellow
+    git config --global user.email '33888807+camusicjunkie@users.noreply.github.com'
+    git config --global user.name 'John Steele'
+    git config --global credential.helper "store --file ~/.git-credentials"
+
     Set-BuildEnvironment -Force
+
     $env:BHBuildModulePath = "$env:BHBuildOutput\$env:BHProjectName\$env:BHProjectName.psm1"
     $env:BHBuildManifestPath = "$env:BHBuildOutput\$env:BHProjectName\$env:BHProjectName.psd1"
+}
+
+Task GetNextVersion {
+    use { "$env:BHBuildOutput\downloads\Gitversion.Commandline\tools" } gitversion
+
+    $gitversion = exec {gitversion | ConvertFrom-Json}
+    $env:GetNextVersion = $gitversion.MajorMinorPatch
 }
 
 Task ShowInfo GetNextVersion, {
@@ -76,81 +61,12 @@ Task ShowInfo GetNextVersion, {
     Write-Build Gray
 }
 
-Task GetNextVersion {
-    $version = [version] '0.1.0'
-    $publishedModule = $null
-    $bumpVersionType = 'Patch'
-    $versionStamp = (git rev-parse origin/master) + (git rev-parse head)
-
-    Write-Build Gray "Load current version"
-    [version] $sourceVersion = (Get-Metadata -Path $env:BHPSModuleManifest)
-    #"  Source version [$sourceVersion]"
-
-    $downloadFolder = Join-Path -Path $env:BHBuildOutput downloads
-    $null = New-Item -ItemType Directory -Path $downloadFolder -Force -ErrorAction Ignore
-
-    $versionFile = Join-Path $downloadFolder versionfile
-    if (Test-Path $versionFile) {
-        Write-Build Gray "$versionFile exists"
-        $versionFileData = Get-Content $versionFile -raw
-        if ($versionFileData -eq $versionStamp) {
-            Write-Build Yellow 'Exiting task. Version file matches'
-            continue
-        }
-    }
-
-    Write-Build Gray -Text 'Checking for published version'
-    $publishedModule = Find-Module -Name $env:BHProjectName -ErrorAction Ignore
-
-    if ($null -ne $publishedModule) {
-        [version] $publishedVersion = $publishedModule.Version
-        Write-Build "  Published version [$publishedVersion]" -Color Cyan
-
-        $version = $publishedVersion
-
-        #"Downloading published module to check for breaking changes"
-        $publishedModule | Save-Module -Path $downloadFolder
-
-        [System.Collections.Generic.HashSet[string]] $publishedInterface = @(Get-ModuleInterfaceMap -Path (Join-Path $downloadFolder $env:BHProjectName))
-        [System.Collections.Generic.HashSet[string]] $buildInterface = @(Get-ModuleInterfaceMap -Path $env:BHPSModuleManifest)
-
-        $bumpVersionType = if (-not $publishedInterface.IsSubsetOf($buildInterface)) {
-            'Major'
-        }
-        elseif ($publishedInterface.count -ne $buildInterface.count) {
-            'Minor'
-        }
-    }
-
-    if ($version -lt ([version] '1.0.0')) {
-        #"Module is still in beta; don't bump major version."
-        $bumpVersionType = if ($bumpVersionType -eq 'Major') { 'Minor' } else { 'Patch' }
-    }
-
-    #"  Stepping version [$bumpVersionType]"
-    $version = [version] (Step-Version -Version $version -Type $bumpVersionType)
-
-    #"  Comparing to source version [$sourceVersion]"
-    if ($sourceVersion -gt $version) {
-        #"    Using existing version"
-        $version = $sourceVersion
-    }
-
-    if ( -not [string]::IsNullOrEmpty($env:Build_BuildID)) {
-        #$build = $env:Build_BuildID
-        #$version = [version]::new($version.Major, $version.Minor, $version.Build, $build)
-    }
-    elseif ( -not [string]::IsNullOrEmpty($env:APPVEYOR_BUILD_ID)) {
-        #$build = $env:APPVEYOR_BUILD_ID
-        #$version = [version]::new($version.Major, $version.Minor, $version.Build, $build)
-    }
-
-    #"  Setting version [$version]"
-    # Update-Metadata -Path $env:BHPSModuleManifest -Value $version
-
-    # Get-Content -Path $env:BHPSModuleManifest -Raw -Encoding UTF8 |
-    #     ForEach-Object { $_.TrimEnd() } |
-    #     Set-Content -Path $env:BHPSModuleManifest -Encoding UTF8
-
-    #Set-Content -Path $versionFile -Value $versionStamp -NoNewline -Encoding UTF8
+Task Clean -If (Get-ChildItem $env:BHBuildOutput -Exclude downloads, modules) {
+    remove (Get-ChildItem $env:BHBuildOutput -Exclude downloads, modules)
 }
+
+Task . ShowInfo #, Clean, Build, Test
+
+# Task Test
+
+# Task Deploy
