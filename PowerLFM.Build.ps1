a6736b71c40576f1b17ab6a0efdd94ca6567d5f3
John Steele <33888807+camusicjunkie@users.noreply.github.com>
2020-01-10 13:10:39 -0700

    switch slashes; add generate external help task

--- PowerLFM.Build.ps1
+++ PowerLFM.Build.ps1
@@ -23,7 +23,7 @@ if ($PSBoundParameters.ContainsKey('Debug')) {
 Enter-Build {
     git config --global user.email '33888807+camusicjunkie@users.noreply.github.com'
     git config --global user.name 'John Steele'
-    git config --global credential.helper "store --file ~/.git-credentials"
+    git config --global credential.helper "store --file ~\.git-credentials"
 
     Set-BuildEnvironment -Force
 
@@ -63,26 +63,50 @@ Task ShowInfo GetNextVersion, {
     Write-Build Gray
 }
 
-Task Build CopyModuleFiles, CompileModule
+Task Build CopyModuleFiles, UpdateManifest, CompileModule, CleanBuild
+
+# Synopsis: Generate external help for each public function
+task GenerateExternalHelp {
+    $neParams = @{
+        Path = "$env:BHProjectPath\docs"
+        OutputPath = "$env:BHBuildOutput\$env:BHProjectName\$PSCulture"
+        Force = $true
+    }
+    $null = New-ExternalHelp @neParams
+}
 
 # Synopsis: Copy module files over to build output folder
 task CopyModuleFiles {
     # Setup
-    if (-not (Test-Path "$env:BHBuildOutput/$env:BHProjectName")) {
-        $null = New-Item -Path "$env:BHBuildOutput/$env:BHProjectName" -ItemType Directory
+    if (-not (Test-Path "$env:BHBuildOutput\$env:BHProjectName")) {
+        $null = New-Item -Path "$env:BHBuildOutput\$env:BHProjectName" -ItemType Directory
     }
 
     # Copy module
-    Copy-Item -Path "$env:BHModulePath/*" -Destination "$env:BHBuildOutput/$env:BHProjectName" -Recurse -Force
+    Copy-Item -Path "$env:BHModulePath\*" -Destination "$env:BHBuildOutput\$env:BHProjectName" -Recurse -Force
 
     # Copy additional files
     Copy-Item -Path @(
-        #"$env:BHProjectPath/CHANGELOG.md"
-        "$env:BHProjectPath/LICENSE"
-        "$env:BHProjectPath/README.md"
-    ) -Destination "$env:BHBuildOutput/$env:BHProjectName" -Force
+        "$env:BHProjectPath\LICENSE"
+        "$env:BHProjectPath\README.md"
+    ) -Destination "$env:BHBuildOutput\$env:BHProjectName" -Force
 
     Invoke-PSDepend -Tags Copy -Confirm:$false
+}
+
+# Synopsis: Update the manifest of the module
+task UpdateManifest GetNextVersion, {
+    Remove-Module $env:BHProjectName -ErrorAction SilentlyContinue
+    Import-Module $env:BHPSModuleManifest -Force
+
+    $public = @(Get-ChildItem -Path "$env:BHBuildOutput\$env:BHProjectName\Public\*.ps1" -ErrorAction SilentlyContinue)
+
+    $ummParams = @{
+        Path = $env:BHBuildManifestPath
+        FunctionsToExport = $public.BaseName
+        ModuleVersion = $env:NextBuildVersion
+    }
+    Update-ModuleManifest @ummParams
 }
 
 # Synopsis: Compile all functions into the .psm1 file
@@ -93,26 +117,28 @@ task CompileModule {
     $capture = $false
     $compiled = ''
 
-    #captures all content between #region/#endregion tags in $regionsToKeep
+    #captures all content between #region\#endregion tags in $regionsToKeep
     $compiled = foreach ($line in $content) {
         if ($capture) { $line -replace '^#endregion.+'}
         if ($line -match "^#region ($($regionsToKeep -join "|"))$") { $capture = $true }
         if (($capture -eq $true) -and ($line -match "^#endregion")) { $capture = $false }
     }
 
-    $public = @(Get-ChildItem -Path "$env:BHBuildOutput/$env:BHProjectName/Public/*.ps1" -ErrorAction SilentlyContinue)
-    $private = @(Get-ChildItem -Path "$env:BHBuildOutput/$env:BHProjectName/Private/*.ps1" -ErrorAction SilentlyContinue)
+    $public = @(Get-ChildItem -Path "$env:BHBuildOutput\$env:BHProjectName\Public\*.ps1" -ErrorAction SilentlyContinue)
+    $private = @(Get-ChildItem -Path "$env:BHBuildOutput\$env:BHProjectName\Private\*.ps1" -ErrorAction SilentlyContinue)
 
     $content = foreach ($function in @($public + $private)) {
         Get-Content -Path $function.FullName -Raw
     }
 
     Set-Content -LiteralPath $env:BHBuildModulePath -Value @($compiled, $content) -Encoding UTF8 -Force -Exclude '^#region'
+}
 
+task CleanBuild CompileModule, {
     "Private", "Public" | Foreach-Object { Remove-Item -Path "$env:BHBuildOutput/$env:BHProjectName/$_" -Recurse -Force }
 }
 
-Task Clean -If (Get-ChildItem $env:BHBuildOutput -Exclude downloads, modules) {
+task Clean -If (Get-ChildItem $env:BHBuildOutput -Exclude downloads, modules) {
     remove (Get-ChildItem $env:BHBuildOutput -Exclude downloads, modules)
 }
 
