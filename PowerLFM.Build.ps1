4e2b3cd47262022454abee9a81f89d1d96e276c0
John Steele <33888807+camusicjunkie@users.noreply.github.com>
2020-01-10 01:51:09 -0700

    add copy and compile module tasks

--- PowerLFM.Build.ps1
+++ PowerLFM.Build.ps1
@@ -31,6 +31,7 @@ Enter-Build {
     $env:BHBuildManifestPath = "$env:BHBuildOutput\$env:BHProjectName\$env:BHProjectName.psd1"
 }
 
+# Synopsis: Get the next build version
 Task GetNextVersion {
     use "$env:BHBuildOutput\downloads\GitVersion.CommandLine\tools" gitversion
 
@@ -38,6 +39,7 @@ Task GetNextVersion {
     $env:NextBuildVersion = $gitversion.MajorMinorPatch
 }
 
+# Synopsis: Display build information
 Task ShowInfo GetNextVersion, {
     Write-Build Gray
     Write-Build Gray ('Running in:                 {0}' -f $env:BHBuildSystem)
@@ -59,6 +61,54 @@ Task ShowInfo GetNextVersion, {
     #Write-Build Gray ('OS:                         {0}' -f $OS)
     #Write-Build Gray ('OS Version:                 {0}' -f $OSVersion)
     Write-Build Gray
+}
+
+# Synopsis: Copy module files over to build output
+task CopyModuleFiles {
+    # Setup
+    if (-not (Test-Path "$env:BHBuildOutput/$env:BHProjectName")) {
+        $null = New-Item -Path "$env:BHBuildOutput/$env:BHProjectName" -ItemType Directory
+    }
+
+    # Copy module
+    Copy-Item -Path "$env:BHModulePath/*" -Destination "$env:BHBuildOutput/$env:BHProjectName" -Recurse -Force
+    # Copy additional files
+    Copy-Item -Path @(
+        #"$env:BHProjectPath/CHANGELOG.md"
+        "$env:BHProjectPath/LICENSE"
+        "$env:BHProjectPath/README.md"
+    ) -Destination "$env:BHBuildOutput/$env:BHProjectName" -Force
+
+    Invoke-PSDepend -Tags Copy
+}
+
+# Synopsis: Compile all functions into the .psm1 file
+task CompileModule {
+    $regionsToKeep = @('Init')
+
+    $content = Get-Content -Encoding UTF8 -LiteralPath $env:BHBuildModulePath
+    $capture = $false
+    $compiled = ""
+
+    foreach ($line in $content) {
+        if ($line -match "^#region ($($regionsToKeep -join "|"))$") { $capture = $true }
+
+        if (($capture -eq $true) -and ($line -match "^#endregion")) { $capture = $false }
+
+        if ($capture) { $compiled += "$line`r`n" }
+    }
+
+    $public = @( Get-ChildItem -Path "$env:BHBuildOutput/$env:BHProjectName/Public/*.ps1" -ErrorAction SilentlyContinue )
+    $private = @( Get-ChildItem -Path "$env:BHBuildOutput/$env:BHProjectName/Private/*.ps1" -ErrorAction SilentlyContinue )
+
+    foreach ($function in @($public + $private)) {
+        $compiled += (Get-Content -Path $function.FullName -Raw)
+        $compiled += "`r`n"
+    }
+
+    Set-Content -LiteralPath $env:BHBuildModulePath -Value $compiled -Encoding UTF8 -Force
+
+    "Private", "Public" | Foreach-Object { Remove-Item -Path "$env:BHBuildOutput/$env:BHProjectName/$_" -Recurse -Force }
 }
 
 Task Clean -If (Get-ChildItem $env:BHBuildOutput -Exclude downloads, modules) {
