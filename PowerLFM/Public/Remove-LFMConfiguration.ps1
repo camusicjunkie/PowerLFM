13b8bf5f7c642aa7e75d221bc8fe9430ca2c2785
John Steele <33888807+camusicjunkie@users.noreply.github.com>
2020-07-15 01:10:41 +0200

    added support for secret management

--- PowerLFM/Public/Remove-LFMConfiguration.ps1
+++ PowerLFM/Public/Remove-LFMConfiguration.ps1
@@ -2,13 +2,17 @@ function Remove-LFMConfiguration {
     # .ExternalHelp PowerLFM-help.xml
 
     [CmdletBinding(SupportsShouldProcess,
-                   ConfirmImpact = 'Medium')]
+                   ConfirmImpact = 'High')]
     param ()
 
     process {
         try {
-            if ($PSCmdlet.ShouldProcess('Password Vault', 'Removing configuration')) {
-                Remove-LFMVaultCredential
+            $secrets = Get-SecretInfo -Name LFM* -Vault BuiltInLocalVault
+
+            foreach ($secret in $secrets) {
+                if ($PSCmdlet.ShouldProcess('BuiltInLocalVault', "Removing secret: $($secret.Name)")) {
+                    Remove-Secret -Name $secret.Name -Vault BuiltInLocalVault -Verbose
+                }
             }
         } catch {
             throw $_
