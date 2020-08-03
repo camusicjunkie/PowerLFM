c5447c16d9387e4b04c15f145c8493a36bbda3a2
John Steele <33888807+camusicjunkie@users.noreply.github.com>
2020-08-03 22:47:36 +0200

    brain melting updates for v5

--- Tests/Get-LFMAlbumTag.Tests.ps1
+++ Tests/Get-LFMAlbumTag.Tests.ps1
@@ -1,10 +1,12 @@
-Remove-Module -Name PowerLFM -ErrorAction Ignore
-Import-Module -Name $PSScriptRoot\..\PowerLFM\PowerLFM.psd1
+BeforeAll {
+    Remove-Module -Name PowerLFM -ErrorAction Ignore
+    Import-Module -Name $PSScriptRoot\..\PowerLFM\PowerLFM.psd1
+}
 
 Describe 'Get-LFMAlbumTag: Interface' -Tag Interface {
 
     BeforeAll {
-        $script:command = (Get-Command -Name 'Get-LFMAlbumTag')
+        $command = (Get-Command -Name 'Get-LFMAlbumTag')
     }
 
     It 'CmdletBinding should be declared' {
@@ -25,11 +27,15 @@ Describe 'Get-LFMAlbumTag: Interface' -Tag Interface {
             $command.ParameterSets.Name | Should -Contain 'album'
         }
 
-        $parameterSet = $command.ParameterSets | Where-Object Name -eq album
+        BeforeAll {
+            $parameterSet = $command.ParameterSets | Where-Object Name -eq album
+        }
 
         Context 'Parameter [Album] attribute validation' {
 
-            $parameter = $parameterSet.Parameters | Where-Object Name -eq Album
+            BeforeAll {
+                $parameter = $parameterSet.Parameters | Where-Object Name -eq Album
+            }
 
             It 'Should not be null or empty' {
                 $parameter | Should -Not -BeNullOrEmpty
@@ -62,7 +68,9 @@ Describe 'Get-LFMAlbumTag: Interface' -Tag Interface {
 
         Context 'Parameter [Artist] attribute validation' {
 
-            $parameter = $parameterSet.Parameters | Where-Object Name -eq Artist
+            BeforeAll {
+                $parameter = $parameterSet.Parameters | Where-Object Name -eq Artist
+            }
 
             It 'Should not be null or empty' {
                 $parameter | Should -Not -BeNullOrEmpty
@@ -95,7 +103,9 @@ Describe 'Get-LFMAlbumTag: Interface' -Tag Interface {
 
         Context 'Parameter [UserName] attribute validation' {
 
-            $parameter = $parameterSet.Parameters | Where-Object Name -eq UserName
+            BeforeAll {
+                $parameter = $parameterSet.Parameters | Where-Object Name -eq UserName
+            }
 
             It 'Should not be null or empty' {
                 $parameter | Should -Not -BeNullOrEmpty
@@ -128,7 +138,9 @@ Describe 'Get-LFMAlbumTag: Interface' -Tag Interface {
 
         Context 'Parameter [AutoCorrect] attribute validation' {
 
-            $parameter = $parameterSet.Parameters | Where-Object Name -eq AutoCorrect
+            BeforeAll {
+                $parameter = $parameterSet.Parameters | Where-Object Name -eq AutoCorrect
+            }
 
             It 'Should not be null or empty' {
                 $parameter | Should -Not -BeNullOrEmpty
@@ -166,11 +178,15 @@ Describe 'Get-LFMAlbumTag: Interface' -Tag Interface {
             $command.ParameterSets.Name | Should -Contain 'id'
         }
 
-        $parameterSet = $command.ParameterSets | Where-Object Name -eq id
+        BeforeAll {
+            $parameterSet = $command.ParameterSets | Where-Object Name -eq id
+        }
 
         Context 'Parameter [Id] attribute validation' {
 
-            $parameter = $parameterSet.Parameters | Where-Object Name -eq Id
+            BeforeAll {
+                $parameter = $parameterSet.Parameters | Where-Object Name -eq Id
+            }
 
             It 'Should not be null or empty' {
                 $parameter | Should -Not -BeNullOrEmpty
@@ -203,7 +219,9 @@ Describe 'Get-LFMAlbumTag: Interface' -Tag Interface {
 
         Context 'Parameter [UserName] attribute validation' {
 
-            $parameter = $parameterSet.Parameters | Where-Object Name -eq UserName
+            BeforeAll {
+                $parameter = $parameterSet.Parameters | Where-Object Name -eq UserName
+            }
 
             It 'Should not be null or empty' {
                 $parameter | Should -Not -BeNullOrEmpty
@@ -236,7 +254,9 @@ Describe 'Get-LFMAlbumTag: Interface' -Tag Interface {
 
         Context 'Parameter [AutoCorrect] attribute validation' {
 
-            $parameter = $parameterSet.Parameters | Where-Object Name -eq AutoCorrect
+            BeforeAll {
+                $parameter = $parameterSet.Parameters | Where-Object Name -eq AutoCorrect
+            }
 
             It 'Should not be null or empty' {
                 $parameter | Should -Not -BeNullOrEmpty
@@ -269,111 +289,124 @@ Describe 'Get-LFMAlbumTag: Interface' -Tag Interface {
     }
 }
 
-InModuleScope PowerLFM {
+Describe 'Get-LFMAlbumTag: Unit' -Tag Unit {
 
-    $mocks = Get-Content -Path $PSScriptRoot\..\config\mocks.json | ConvertFrom-Json
-    $contextMock = $mocks.'Get-LFMAlbumTag'.AlbumTag
-
-    Describe 'Get-LFMAlbumTag: Unit' -Tag Unit {
-
+    BeforeAll {
         Mock Remove-CommonParameter {
             [hashtable] @{
-                Album = 'Album'
+                Album  = 'Album'
                 Artist = 'Artist'
             }
+        } -ModuleName 'PowerLFM'
+        Mock ConvertTo-LFMParameter -ModuleName 'PowerLFM'
+        Mock New-LFMApiQuery -ModuleName 'PowerLFM'
+        Mock Invoke-LFMApiUri { $contextMock } -ModuleName 'PowerLFM'
+    }
+
+    Context 'Input' {
+
+        It 'Should throw when album is null' {
+            { Get-LFMAlbumTag -Album $null } | Should -Throw
         }
-        Mock ConvertTo-LFMParameter
-        Mock New-LFMApiQuery
-        Mock Invoke-LFMApiUri {$contextMock}
+    }
 
-        Context 'Input' {
+    Context 'Execution' {
 
-            It 'Should throw when album is null' {
-                {Get-LFMAlbumTag -Album $null} | Should -Throw
-            }
-        }
-
-        Context 'Execution' {
-
+        BeforeAll {
             Get-LFMAlbumTag -Album Album -Artist Artist
-
-            It 'Should remove common parameters from bound parameters' {
-                $amParams = @{
-                    CommandName     = 'Remove-CommonParameter'
-                    Exactly         = $true
-                    Times           = 1
-                    ParameterFilter = {
-                        $PSBoundParameters
-                    }
-                }
-                Assert-MockCalled @amParams
-            }
-
-            It 'Should convert parameters to format API expects after signing' {
-                $amParams = @{
-                    CommandName = 'ConvertTo-LFMParameter'
-                    Exactly     = $true
-                    Times       = 1
-                }
-                Assert-MockCalled @amParams
-            }
-
-            It 'Should take hashtable and build a query for a uri' {
-                $amParams = @{
-                    CommandName = 'New-LFMApiQuery'
-                    Exactly     = $true
-                    Times       = 1
-                }
-                Assert-MockCalled @amParams
-            }
         }
 
-        Context 'Output' {
+        It 'Should remove common parameters from bound parameters' {
+            $amParams = @{
+                CommandName     = 'Remove-CommonParameter'
+                ModuleName      = 'PowerLFM'
+                Scope           = 'Context'
+                Exactly         = $true
+                Times           = 1
+                ParameterFilter = {
+                    $PSBoundParameters
+                }
+            }
+            Assert-MockCalled @amParams
+        }
+
+        It 'Should convert parameters to format API expects after signing' {
+            $amParams = @{
+                CommandName = 'ConvertTo-LFMParameter'
+                ModuleName  = 'PowerLFM'
+                Scope       = 'Context'
+                Exactly     = $true
+                Times       = 1
+            }
+            Assert-MockCalled @amParams
+        }
+
+        It 'Should take hashtable and build a query for a uri' {
+            $amParams = @{
+                CommandName = 'New-LFMApiQuery'
+                ModuleName  = 'PowerLFM'
+                Scope       = 'Context'
+                Exactly     = $true
+                Times       = 1
+            }
+            Assert-MockCalled @amParams
+        }
+    }
+
+    Context 'Output' {
+
+        $mocks = Get-Content -Path $PSScriptRoot\..\config\mocks.json | ConvertFrom-Json
+        $contextMock = $mocks.'Get-LFMAlbumTag'.AlbumTag
+
+        BeforeAll {
+            $mocks = Get-Content -Path $PSScriptRoot\..\config\mocks.json | ConvertFrom-Json
+            $contextMock = $mocks.'Get-LFMAlbumTag'.AlbumTag
 
             $output = Get-LFMAlbumTag -Album Album -Artist Artist
+        }
 
-            It "Album first tag should have name of $($contextMock.Tags.Tag[0].Name)" {
-                $output[0].Tag | Should -Be $contextMock.Tags.Tag[0].Name
-            }
+        It "Album first tag should have name of $($contextMock.Tags.Tag[0].Name)" {
+            $output[0].Tag | Should -Be $contextMock.Tags.Tag[0].Name
+        }
 
-            It "Album second tag should have url of $($contextMock.Tags.Tag[1].Url)" {
-                $output[1].Url | Should -Be $contextMock.Tags.Tag[1].Url
-            }
+        It "Album second tag should have url of $($contextMock.Tags.Tag[1].Url)" {
+            $output[1].Url | Should -Be $contextMock.Tags.Tag[1].Url
+        }
 
-            It 'Album should have two tags' {
-                $output.Tag | Should -HaveCount 2
-            }
+        It 'Album should have two tags' {
+            $output.Tag | Should -HaveCount 2
+        }
 
-            It 'Album should not have more than two tags' {
-                $output.Tag | Should -Not -BeNullOrEmpty
-                $output.Tag | Should -Not -HaveCount 3
-            }
+        It 'Album should not have more than two tags' {
+            $output.Tag | Should -Not -BeNullOrEmpty
+            $output.Tag | Should -Not -HaveCount 3
+        }
 
-            It 'Album should have two tags when id parameter is used' {
-                $output = Get-LFMAlbumTag -Id (New-Guid)
-                $output.Tags | Should -HaveCount 2
-            }
+        It 'Album should have two tags when id parameter is used' {
+            $output = Get-LFMAlbumTag -Id (New-Guid)
+            $output.Tags | Should -HaveCount 2
+        }
 
-            It 'Should call the correct Last.fm get method' {
-                Get-LFMAlbumTag -Album Album -Artist Artist
+        It 'Should call the correct Last.fm get method' {
+            Get-LFMAlbumTag -Album Album -Artist Artist
 
-                $amParams = @{
-                    CommandName = 'Invoke-LFMApiUri'
-                    Exactly = $true
-                    Times = 1
-                    Scope = 'It'
-                    ParameterFilter = {
-                        $Uri -like 'https://ws.audioscrobbler.com/2.0*'
-                    }
+            $amParams = @{
+                CommandName     = 'Invoke-LFMApiUri'
+                ModuleName      = 'PowerLFM'
+                Exactly         = $true
+                Times           = 1
+                Scope           = 'It'
+                ParameterFilter = {
+                    $Uri -like 'https://ws.audioscrobbler.com/2.0*'
                 }
-                Assert-MockCalled @amParams
             }
+            Assert-MockCalled @amParams
+        }
 
-            It 'Should throw when an error is returned in the response' {
-                Mock Invoke-LFMApiUri { throw 'Error' }
+        It 'Should throw when an error is returned in the response' {
+            Mock Invoke-LFMApiUri { throw 'Error' } -ModuleName 'PowerLFM'
 
-                { Get-LFMAlbumTag -Album Album -Artist Artist } | Should -Throw 'Error'
-            }
+            { Get-LFMAlbumTag -Album Album -Artist Artist } | Should -Throw 'Error'
         }
     }
 }
