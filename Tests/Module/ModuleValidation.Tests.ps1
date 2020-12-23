ebdc1ffe0b4fd90401aa393bb50874ba8adf6d84
John Steele <33888807+camusicjunkie@users.noreply.github.com>
2020-12-23 05:11:35 +0100

    updated for pester 5 compatibility

--- Tests/Module/ModuleValidation.Tests.ps1
+++ Tests/Module/ModuleValidation.Tests.ps1
@@ -1,26 +1,36 @@
 Describe 'Static Analysis: Module & Repository Files' {
 
-    #region Discovery
+    BeforeDiscovery {
+        $moduleName = 'PowerLFM'
+        $projectRoot = Resolve-Path "$PSScriptRoot\..\.."
+        $moduleManifestPath = "$projectRoot\$moduleName\$moduleName.psd1"
 
-    $moduleName = 'PowerLFM'
-    $projectRoot = Resolve-Path "$PSScriptRoot\..\.."
+        $fileSearch = @{
+            Path    = Resolve-Path $projectRoot\$moduleName
+            Include = '*.ps1', '*.psm1', '*.psd1'
+            Recurse = $true
+        }
+        $scripts = Get-ChildItem @fileSearch
 
-    $fileSearch = @{
-        Path    = Resolve-Path $projectRoot\$moduleName
-        Include = '*.ps1', '*.psm1', '*.psd1'
-        Recurse = $true
+        $testCases = $scripts | ForEach-Object { @{ FileName = $_.BaseName; FullName = $_.FullName } }
+
+        Import-Module $moduleManifestPath
+        $module = Get-Module $moduleName
+        $commands = @(
+            $module.ExportedFunctions.Keys
+            $module.ExportedCmdlets.Keys
+        )
     }
-    $scripts = Get-ChildItem @fileSearch
-    $testCases = $scripts | ForEach-Object { @{ FileName = $_.BaseName; FullName = $_.FullName } }
 
-    $moduleManifestPath = "$projectRoot\$moduleName\$moduleName.psd1"
-    Import-Module $moduleManifestPath
-
-    #endregion Discovery
+    BeforeAll {
+        $moduleName = 'PowerLFM'
+        $projectRoot = Resolve-Path "$PSScriptRoot\..\.."
+        $moduleManifestPath = "$projectRoot\$moduleName\$moduleName.psd1"
+    }
 
     Context 'Repository code' {
 
-        It 'Should have no invalid syntax errors in <FileName>' -TestCases $testCases {
+        It 'Should have no invalid syntax errors in <FileName>' -ForEach $testCases {
             $FullName | Should -Exist
 
             $fileContents = Get-Content -Path $FullName -ErrorAction Stop
@@ -29,19 +39,13 @@ Describe 'Static Analysis: Module & Repository Files' {
             $errors.Count | Should -Be 0
         }
 
-        It 'Should have exactly one line feed at EOF in <FileName>' -TestCases $testCases {
+        It 'Should have exactly one line feed at EOF in <FileName>' -ForEach $testCases {
             $crlf = [Regex]::Match((Get-Item $FullName | Get-Content -Raw), '(\r?(?<lf>\n))+\Z')
             $crlf.Groups['lf'].Captures.Count | Should -Be 1
         }
     }
 
     Context 'Module import' {
-
-        BeforeAll {
-            $moduleName = 'PowerLFM'
-            $projectRoot = Resolve-Path "$PSScriptRoot\..\.."
-            $moduleManifestPath = "$projectRoot\$moduleName\$moduleName.psd1"
-        }
 
         It 'Should cleanly import the module' {
             { Import-Module $moduleManifestPath -Force } | Should -Not -Throw
@@ -59,20 +63,12 @@ Describe 'Static Analysis: Module & Repository Files' {
 
     Context 'Test files' {
 
-        #region Discovery
-
-        $module = Get-Module $moduleName
-        $commands = @($module.ExportedFunctions.Keys)
-        $testCases = $commands | ForEach-Object {  @{ FileName = $_ } }
-
-        #endregion Discovery
-
-        BeforeAll {
-            $projectRoot = Resolve-Path "$PSScriptRoot\..\.."
+        BeforeEach {
+            $fileName = $_
         }
 
-        It 'Should have a unit test for <FileName>' -TestCases $testCases {
-            Get-Content "$projectRoot\Tests\$FileName.tests.ps1" | Should -Not -BeNullOrEmpty
+        It 'Should have a unit test for <_>' -ForEach $commands {
+            Get-Content "$projectRoot\Tests\$fileName.tests.ps1" | Should -Not -BeNullOrEmpty
         }
     }
 }
